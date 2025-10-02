087221a58949f66f07b5c557a58292780ac692e2
Wrench56 <dmarkreg@gmail.com>
2025-10-02 11:32:50 -0400

    feat(efi): add v-table init and integrate it into `main.asm`

--- src/efi_funcs.asm
+++ src/efi_funcs.asm
@@ -1,8 +1,8 @@
 ; ============================================= ;
-;  > main.asm                                   ;
+;  > efi_funcs.asm                              ;
 ; --------------------------------------------- ;
 ;                                               ;
-;  Entry point for EFI.                         ;
+;  Loads UEFI functions into a table.           ;
 ;                                               ;
 ;  Author(s)  : Mark Devenyi                    ;
 ;  Created    :  2 Oct 2025                     ;
@@ -17,7 +17,9 @@
 ; --------------------------------------------- ;
 ;                                               ;
 ;  Exports:                                     ;
-;   > [F] efi_main                              ;
+;   > [F] efi_init_func_table                   ;
+;                                               ;
+;   > [P] efi_funcs                             ;
 ;                                               ;
 ; ============================================= ;
 
@@ -27,14 +29,15 @@ bits 64
 %include "efilib.inc"
 %include "efi_funcs.inc"
 
-extern efi_init_func_table
-extern efi_funcs
+section .bss
+    global efi_funcs
+    efi_funcs: resb sizeof(efi_func_table)
 
 
 section .text
 
 ; ============================================= ;
-;  > efi_main                                   ;
+;  > efi_init_func_table                        ;
 ; --------------------------------------------- ;
 ;                                               ;
 ;  Entry point of EFI.                          ;
@@ -44,33 +47,33 @@ section .text
 ;  Updated    :  2 Oct 2025                     ;
 ;  Extensions : None                            ;
 ;  Libraries  : None                            ;
-;  ABI used   : Microsoft x64                   ;
+;  ABI used   : Microsoft x64 / custom          ;
 ;                                               ;
 ; --------------------------------------------- ;
 ;                                               ;
 ;  Scope      : Global                          ;
-;  Effects    : None                            ;
+;  Effects    : Does NOT change RDX or RCX but  ;
+;               does change R8, R10, R11        ;
 ;                                               ;
 ;  Returns:                                     ;
-;   int                                         ;
+;   void                                        ;
 ;                                               ;
 ;  Arguments:                                   ;
 ;   > RCX - void* handle                        ;
 ;   > RDX - efi_system_table* system_table      ;
 ;                                               ;
 ; ============================================= ;
-global efi_main
-efi_main:
-    push            rbp
-    push            r15
-    sub             rsp, 32 + 8
+global efi_init_func_table
+efi_init_func_table:
+    lea             r8, [rel efi_funcs]
 
-    mov             rbp, rdx
-    call            efi_init_func_table
-    lea             r15, [rel efi_funcs]
+    ; Simple text output protocol functions
+    mov             r10, [rdx + efi_system_table.ConOut]
+    mov             r11, [r10 + efi_simple_text_output_protocol.ClearScreen]
+    mov             [r8 + efi_func_table.ClearScreen], r11
+    mov             r11, [r10 + efi_simple_text_output_protocol.OutputString]
+    mov             [r8 + efi_func_table.OutputString], r11
+    mov             r11, [r10 + efi_simple_text_output_protocol.SetCursorPosition]
+    mov             [r8 + efi_func_table.SetCursorPosition], r11
 
-    xor             rax, rax
-    add             rsp, 32 + 8
-    pop             r15
-    pop             rbp
     ret
