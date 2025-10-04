bed00e1ecdb154f346d5f9618681231dac1e219b
Mark Devenyi <markdevenyidev@gmail.com>
2025-10-04 10:59:06 -0400

    feat(hashmap): add `hash_callback` and wire into `hashmap_init()`

--- src/hashmap/hashmap.asm
+++ src/hashmap/hashmap.asm
@@ -7,7 +7,7 @@
 ;                                               ;
 ;  Author(s)  : Mark Devenyi                    ;
 ;  Created    :  2 Oct 2025                     ;
-;  Updated    :  2 Oct 2025                     ;
+;  Updated    :  4 Oct 2025                     ;
 ;  Version    : 1.0.0                           ;
 ;  License    : MIT                             ;
 ;  Libraries  : None                            ;
@@ -37,7 +37,7 @@ section .text
 ;                                               ;
 ;  Author(s)  : Mark Devenyi                    ;
 ;  Created    :  2 Oct 2025                     ;
-;  Updated    :  2 Oct 2025                     ;
+;  Updated    :  4 Oct 2025                     ;
 ;  Extensions : None                            ;
 ;  Libraries  : None                            ;
 ;  ABI used   : Microsoft x64                   ;
@@ -52,6 +52,8 @@ section .text
 ;                                               ;
 ;  Arguments:                                   ;
 ;    > RCX - ptr eq_callback_function           ;
+;    > RDX - ptr hash_callback_function         ;
+;                                               ;
 ; ============================================= ;
 global hashmap_init
 hashmap_init:
@@ -59,13 +61,18 @@ hashmap_init:
     push            r15
     sub             rsp, 32 + 8
 
-    ; Save eq_callback
-    mov             r14, rcx
+    ; Save eq_callback & hash_callback
+    lea             r14, [rcx]
+    lea             r15, [rdx]
 
     mov             rcx, sizeof(hashmap_t)
     call            malloc
     test            rax, rax
     je              .error
+
+    mov             qword [rax + hashmap_t.size], 0
+    mov             [rax + hashmap_t.eq_callback], rcx
+    mov             [rax + hashmap_t.hash_callback], r15
     mov             r15, rax
 
     mov             rcx, HASHMAP_INITIAL_CAPACITY * 8
@@ -75,10 +82,7 @@ hashmap_init:
     je              .error
     mov             [r15 + hashmap_t.entries], rax
 
-    mov             [r15 + hashmap_t.eq_callback], rcx
-    mov             qword [r15 + hashmap_t.size], 0
-
-    mov             rax, r15
+    lea             rax, [r15]
 
 .exit:
     add             rsp, 32 + 8
