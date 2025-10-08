cba03c1097854b1520ed8aaf79c547807d8e00c8
Mark Devenyi <markdevenyidev@gmail.com>
2025-10-08 01:09:28 -0400

    feat(efi): initialize `core` functions and start interactive UI loop in `main.asm`

--- src/main.asm
+++ src/main.asm
@@ -30,10 +30,21 @@ bits 64
 extern efi_init_func_table
 extern efi_funcs
 
+extern printf_init
+extern printf
+
+extern clearscreen_init
+extern clearscreen
+
+extern getchar_init
+
+extern ui_mainloop
+
 
 section .data
-    header_msg dw __?utf16?__("> Baremetal Hashmap v0.0.1"), 0x000A, 0x000D, __?utf16?__("> Boot done!"), 0x000A, 0x000D, 0x0000
-    ptra dq 0
+    header_msg dw __?utf16?__("> Baremetal Hashmap"), 0x000A, 0x000D, __?utf16?__("> Compiled on "), __?utf16?__(__DATE__), __?utf16?__(" "), __?utf16?__(__TIME__), 0x000A, 0x000D, __?utf16?__("> Source code @ https://github.com/Wrench56/bmhm"), 0x000A, 0x000D, __?utf16?__("> Press H for [H]elp"), 0x000A, 0x000D, 0x0000
+
+
 section .text
 
 ; ============================================= ;
@@ -44,7 +55,7 @@ section .text
 ;                                               ;
 ;  Author(s)  : Mark Devenyi                    ;
 ;  Created    :  2 Oct 2025                     ;
-;  Updated    :  2 Oct 2025                     ;
+;  Updated    :  8 Oct 2025                     ;
 ;  Extensions : None                            ;
 ;  Libraries  : None                            ;
 ;  ABI used   : Microsoft x64                   ;
@@ -68,18 +79,28 @@ efi_main:
     push            r15
     sub             rsp, 32 + 8
 
+    ; Initializations
     mov             rbp, rdx
     call            efi_init_func_table
     lea             r15, [rel efi_funcs]
 
+    mov             rcx, rbp
+    call            printf_init
+
+    mov             rcx, rbp
+    call            getchar_init
+
+    mov             rcx, rbp
+    call            clearscreen_init
+
     ; Clear the screen
-    mov             rcx, [rbp + efi_system_table.ConOut]
-    call            [r15 + efi_func_table.ClearScreen]
+    call            clearscreen
 
     ; Print header
-    mov             rcx, [rbp + efi_system_table.ConOut]
-    lea             rdx, [rel header_msg]
-    call            [r15 + efi_func_table.OutputString]
+    lea             rcx, [rel header_msg]
+    call            printf
+
+    call            ui_mainloop
 
     xor             rax, rax
     add             rsp, 32 + 8
