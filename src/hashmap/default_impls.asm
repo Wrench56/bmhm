1e5c842afc2ce0b5ddc0fd1bf049ec26f69e0cf3
Mark Devenyi <markdevenyidev@gmail.com>
2025-10-08 01:00:21 -0400

    feat(hashmap): add default UTF-16 str-str hashmap callback implementations

---
+++ src/hashmap/default_impls.asm
@@ -0,0 +1,228 @@
+; ============================================= ;
+;  > default_impls.asm                          ;
+; --------------------------------------------- ;
+;                                               ;
+;  Provides sane default callbacks for a        ;
+;  UTF-16 string - UTF-16 string hashmap.       ;
+;                                               ;
+;  Author(s)  : Mark Devenyi                    ;
+;  Created    :  7 Oct 2025                     ;
+;  Updated    :  7 Oct 2025                     ;
+;  Version    : 1.0.0                           ;
+;  License    : MIT                             ;
+;  Libraries  : None                            ;
+;  ABI used   : Microsoft x64                   ;
+;  Arch       : x64/AMD64                       ;
+;  Extensions : None                            ;
+;                                               ;
+; --------------------------------------------- ;
+;                                               ;
+;  Exports:                                     ;
+;   > [F] init_default_hashmap                  ;
+;                                               ;
+; ============================================= ;
+
+
+extern malloc
+extern fnv1a64_hash
+extern printf
+extern hashmap_init
+
+section .text
+
+; ============================================= ;
+;  > hashmap_default_init                       ;
+; --------------------------------------------- ;
+;                                               ;
+;  Create the default hashmap.                  ;
+;                                               ;
+;  Author(s)  : Mark Devenyi                    ;
+;  Created    :  7 Oct 2025                     ;
+;  Updated    :  7 Oct 2025                     ;
+;  Extensions : None                            ;
+;  Libraries  : None                            ;
+;  ABI used   : Microsoft x64                   ;
+;                                               ;
+; --------------------------------------------- ;
+;                                               ;
+;  Scope      : Global                          ;
+;  Effects    : None                            ;
+;                                               ;
+;  Returns:                                     ;
+;   void                                        ;
+;                                               ;
+;  Arguments:                                   ;
+;    > RCX - ptr hashmap_t                      ;
+;                                               ;
+; ============================================= ;
+
+
+global hashmap_default_init
+hashmap_default_init:
+    sub             rsp, 32 + 8
+
+    lea             rcx, [rel eq_callback]
+    lea             rdx, [rel hash_callback]
+    lea             r8, [rel printk_callback]
+    lea             r9, [rel printv_callback]
+    call            hashmap_init
+
+    add             rsp, 32 + 8
+    ret
+
+
+; ============================================= ;
+;  > hash_callback                              ;
+; --------------------------------------------- ;
+;                                               ;
+;  Generates the hash of a UTF-16 key.          ;
+;                                               ;
+;  Author(s)  : Mark Devenyi                    ;
+;  Created    :  7 Oct 2025                     ;
+;  Updated    :  7 Oct 2025                     ;
+;  Extensions : None                            ;
+;  Libraries  : None                            ;
+;  ABI used   : Microsoft x64                   ;
+;                                               ;
+; --------------------------------------------- ;
+;                                               ;
+;  Scope      : Local                           ;
+;  Effects    : None                            ;
+;                                               ;
+;  Returns:                                     ;
+;   uint64 hash                                 ;
+;                                               ;
+;  Arguments:                                   ;
+;    > RCX - char16* key                        ;
+;                                               ;
+; ============================================= ;
+
+hash_callback:
+    push            rax
+    mov             r9, rcx
+    xor             edx, edx
+.hash_len_loop:
+    movzx           eax, word [r9 + rdx*2]
+    test            ax, ax
+    je              .len_done
+    inc             rdx
+    jmp             .hash_len_loop
+.len_done:
+    shl             rdx, 1
+    mov             rcx, r9
+    call            fnv1a64_hash
+    ror             rax, 32
+    pop             rcx
+    ret
+
+
+; ============================================= ;
+;  > eq_callback                                ;
+; --------------------------------------------- ;
+;                                               ;
+;  Compares two keys together. Returns 0 if     ;
+;  they match, non-null if they don't.          ;
+;                                               ;
+;  Author(s)  : Mark Devenyi                    ;
+;  Created    :  7 Oct 2025                     ;
+;  Updated    :  7 Oct 2025                     ;
+;  Extensions : None                            ;
+;  Libraries  : None                            ;
+;  ABI used   : Microsoft x64                   ;
+;                                               ;
+; --------------------------------------------- ;
+;                                               ;
+;  Scope      : Local                           ;
+;  Effects    : None                            ;
+;                                               ;
+;  Returns:                                     ;
+;   uint8 match                                 ;
+;                                               ;
+;  Arguments:                                   ;
+;    > RCX - char16* key1                       ;
+;    > RDX - char16* key2                       ;
+;                                               ;
+; ============================================= ;
+
+eq_callback:
+    cmp     rcx, rdx
+    je      .eq
+.eq_loop:
+    movzx   eax, word [rcx]
+    movzx   r8d, word [rdx]
+    cmp     ax, r8w
+    jne     .ne
+    test    ax, ax
+    je      .eq
+    add     rcx, 2
+    add     rdx, 2
+    jmp     .eq_loop
+.eq:
+    xor     rax, rax
+    ret
+.ne:
+    mov     rax, 1
+    ret
+
+
+; ============================================= ;
+;  > printk_callback                            ;
+; --------------------------------------------- ;
+;                                               ;
+;  Returns the given key string as-is.          ;
+;                                               ;
+;  Author(s)  : Mark Devenyi                    ;
+;  Created    :  7 Oct 2025                     ;
+;  Updated    :  7 Oct 2025                     ;
+;  Extensions : None                            ;
+;  Libraries  : None                            ;
+;  ABI used   : Microsoft x64                   ;
+;                                               ;
+; --------------------------------------------- ;
+;                                               ;
+;  Scope      : Local                           ;
+;  Effects    : None                            ;
+;                                               ;
+;  Returns:                                     ;
+;   char16* string                              ;
+;                                               ;
+;  Arguments:                                   ;
+;    > RCX - char16* key                        ;
+;                                               ;
+; ============================================= ;
+
+printk_callback:
+    mov             rax, rcx
+    ret
+
+
+; ============================================= ;
+;  > printv_callback                            ;
+; --------------------------------------------- ;
+;                                               ;
+;  Returns the given value string as-is.        ;
+;                                               ;
+;  Author(s)  : Mark Devenyi                    ;
+;  Created    :  7 Oct 2025                     ;
+;  Updated    :  7 Oct 2025                     ;
+;  Extensions : None                            ;
+;  Libraries  : None                            ;
+;  ABI used   : Microsoft x64                   ;
+;                                               ;
+; --------------------------------------------- ;
+;                                               ;
+;  Scope      : Local                           ;
+;  Effects    : None                            ;
+;                                               ;
+;  Returns:                                     ;
+;   char16* string                              ;
+;                                               ;
+;  Arguments:                                   ;
+;    > RCX - char16* value                      ;
+;                                               ;
+; ============================================= ;
+
+printv_callback:
+    mov             rax, rcx
+    ret
+
